69829e783f607d6ec8727d622f4192c577f3d317
Артем Погорелов <artem.pog24@gmail.com>
2022-09-20 21:53:15 +0300

    first commit

---
+++ src/test/java/ru/netology/stats/DZStrokiMassivi/service/StatsServiceTest.java
@@ -0,0 +1,67 @@
+package ru.netology.stats.DZStrokiMassivi.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+            public void testsum(){
+    StatsService service=new StatsService();
+    long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+    long actual= service.sum(sales);
+    long expected=180;
+ Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    public void testavarage(){
+        StatsService service=new StatsService();
+        long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual= service.avarage(sales);
+        long expected=180/12;
+        Assertions.assertEquals(expected,actual);
+}
+
+    @Test
+    public void testmaximum(){
+        StatsService service=new StatsService();
+        long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual= service.maximum(sales);
+        long expected=7;
+        Assertions.assertEquals(expected,actual);
+}
+
+    @Test
+    public void testminimum(){
+        StatsService service=new StatsService();
+        long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual= service.minimum(sales);
+        long expected=8;
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void bellowMonths(){
+        StatsService service=new StatsService();
+        long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual= service.bellowMonths(sales);
+        long expected=5;
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void aboveMoths(){
+        StatsService service=new StatsService();
+        long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual= service.aboveMonths(sales);
+        long expected=5;
+        Assertions.assertEquals(expected,actual);
+    }
+}
+
+
