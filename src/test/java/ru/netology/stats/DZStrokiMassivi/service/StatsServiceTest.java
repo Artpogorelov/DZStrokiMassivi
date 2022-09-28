de4f8454b10027351130350b85235689eee90c40
Артем Погорелов <artem.pog24@gmail.com>
2022-09-28 20:19:01 +0300

    fit

--- src/test/java/ru/netology/stats/DZStrokiMassivi/service/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/DZStrokiMassivi/service/StatsServiceTest.java
@@ -5,62 +5,63 @@ import org.junit.jupiter.api.Test;
 
 public class StatsServiceTest {
     @Test
-            public void testsum(){
-    StatsService service=new StatsService();
-    long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    public void testsum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
-    long actual= service.sum(sales);
-    long expected=180;
- Assertions.assertEquals(expected,actual);
-    }
-    @Test
-    public void testavarage(){
-        StatsService service=new StatsService();
-        long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-
-        long actual= service.avarage(sales);
-        long expected=180/12;
-        Assertions.assertEquals(expected,actual);
-}
-
-    @Test
-    public void testmaximum(){
-        StatsService service=new StatsService();
-        long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-
-        long actual= service.maximum(sales);
-        long expected=7;
-        Assertions.assertEquals(expected,actual);
-}
-
-    @Test
-    public void testminimum(){
-        StatsService service=new StatsService();
-        long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-
-        long actual= service.minimum(sales);
-        long expected=8;
-        Assertions.assertEquals(expected,actual);
+        long actual = service.sum(sales);
+        long expected = 180;
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void bellowMonths(){
-        StatsService service=new StatsService();
-        long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    public void testavarage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
-        long actual= service.bellowMonths(sales);
-        long expected=5;
-        Assertions.assertEquals(expected,actual);
+        long actual = service.avarage(sales);
+        long expected = 180 / 12;
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void aboveMoths(){
-        StatsService service=new StatsService();
-        long[]sales={8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    public void testmaximum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
-        long actual= service.aboveMonths(sales);
-        long expected=5;
-        Assertions.assertEquals(expected,actual);
+        long actual = service.maximum(sales);
+        long expected = 7;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testminimum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.minimum(sales);
+        long expected = 8;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void bellowMonths() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.bellowMonths(sales);
+        long expected = 5;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void aboveMoths() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.aboveMonths(sales);
+        long expected = 5;
+        Assertions.assertEquals(expected, actual);
     }
 }
 
